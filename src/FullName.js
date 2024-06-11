60c2bd993582962e97efb0e9524e37b7ec8d1f4c
premnewbie <prempm50@gmail.com>
2024-06-11 19:43:38 +0530

    Created full name form

---
+++ src/FullName.js
@@ -0,0 +1,35 @@
+import { useEffect, useState } from 'react';
+
+const FullName = () => {
+    const [firstName,setFirstName] = useState('');
+    const [lastName,setLastName] = useState('');
+    const [fullName,setFullName] = useState('');
+
+    
+    const handleSetFull = (e) => {
+        e.preventDefault();
+        if(firstName!=='' && lastName!==''){
+            setFullName(firstName+' '+lastName);
+        }
+    }
+
+    return ( <>
+    <h1>Full Name Display</h1>
+    <form>
+        <div>
+            <label>First Name:</label>
+            <input type='text' onInput={(e)=>setFirstName(e.target.value)} required/>
+        </div>
+        <div>
+            <label>Last Name:</label>
+            <input type='text'  onInput={(e)=>setLastName(e.target.value)} required/>
+        </div>
+        <button onClick={(e)=>handleSetFull(e)}>Submit</button>
+    </form>
+    {fullName && <div>
+        Full Name: {fullName}
+    </div>}
+    </> );
+}
+ 
+export default FullName;
